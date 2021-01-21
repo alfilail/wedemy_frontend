b07cd868ca00b88d41d9ee34f9969cdf91efb8ed
Badut IT <badut_it@localhost.localdomain>
2021-01-21 11:55:50 +0700

    chagne name participant to participant-tutor in shared/partials

--- projects/e-learning/src/app/modules/participant-instructor/partial/partial.module.ts
+++ projects/e-learning/src/app/modules/participant-instructor/partial/partial.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from '../../../shared/partials/participant/sidebar/sidebar.component';
-import { HeaderComponent } from '../../../shared/partials/participant/header/header.component';
-import { FooterComponent } from '../../../shared/partials/participant/footer/footer.component';
+import { SidebarComponent } from '../../../shared/partials/participant-tutor/sidebar/sidebar.component';
+import { HeaderComponent } from '../../../shared/partials/participant-tutor/header/header.component';
+import { FooterComponent } from '../../../shared/partials/participant-tutor/footer/footer.component';
 import { RouterModule } from '@angular/router';
 
 
